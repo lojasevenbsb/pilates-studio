cbff01270574028cdf119e97a9f5cb6607c46fad
Glaubert Meireles <lojaseven.bsb@gmail.com>
2026-06-08 19:00:06 -0300

    refactor: remove trailing whitespace and ensure newline at end of file in multiple components

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -17,4 +17,4 @@ export const I = {
   edit:   <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>,
   chev:   <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><polyline points="9 18 15 12 9 6"/></svg>,
   whats:  <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"/></svg>,
-};
+}
--- src/pages/CadastroAluno.jsx
+++ src/pages/CadastroAluno.jsx
@@ -807,4 +807,3 @@ export default function CadastroAluno({ aluno = null, onVoltar, onSalvar }) {
     </div>
   );
 }
-                                                                         
